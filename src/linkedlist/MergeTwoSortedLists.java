957ba9bd7713210e3d5b1b3c55f69b2e49d9cd7f
Codi Chun <codichun@uw.edu>
2022-11-28 11:07:46 -0800

    add mergeTwoLists

--- src/linkedlist/MergeTwoSortedLists.java
+++ src/linkedlist/MergeTwoSortedLists.java
@@ -1,4 +1,7 @@
 package linkedlist;
+
+import java.util.List;
+
 /**
  * 
  * 1,2,4
@@ -19,8 +22,35 @@ public class MergeTwoSortedLists {
 	
 	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 		
-
+		//head-curr-list1
+		
+		ListNode head = new ListNode();
+		ListNode curr = head;
+		
+		while(list1!=null && list2!=null) {
+			if(list1.val<list2.val) {
+				curr.next = list1;
+				curr = curr.next;
+				list1 = list1.next;
+			} else {
+				curr.next = list2;
+				curr = curr.next;
+				list2 = list2.next;
+			}
+		}
+		
+		if(list1!=null) {
+			curr.next = list1;
+		}
+		
+		if(list2!=null) {
+			curr.next = list2;
+		}
+		
+		return head.next;
 	}
+	
+
 	
 
 }
