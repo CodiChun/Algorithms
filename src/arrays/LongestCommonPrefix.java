fea00d9ef554c047a2a0b34a113d5da83124fe34
Codi Chun <codichun@uw.edu>
2022-11-28 11:39:00 -0800

    add lcp

--- src/arrays/LongestCommonPrefix.java
+++ src/arrays/LongestCommonPrefix.java
@@ -1,5 +1,12 @@
 package arrays;
 
+
+/**
+ * ["flower","flow","flight"]
+ * flow -> fl
+ * o(n*s), s is the avg length for strings
+ * o(1)
+ */
 public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
@@ -7,7 +14,14 @@ public class LongestCommonPrefix {
 	}
 	
     public String longestCommonPrefix(String[] strs) {
+    	String lcp = strs[0];
+    	for(String str: strs) {
+    		while(!str.startsWith(lcp)) {
+    			lcp = lcp.substring(0, lcp.length()-1);
+    		}
+    	}
     	
+    	return lcp;
 
         
     }
