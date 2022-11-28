e528d6045ab4042d387ae51eab82c9270b2f802e
Codi Chun <codichun@uw.edu>
2022-11-28 15:00:06 -0800

    add PathSum3

--- src/tree/PathSum3.java
+++ src/tree/PathSum3.java
@@ -6,24 +6,52 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 10,5,3,
- * 18 need 10
+ * map<pathsum, count>
+ * 1,1,2,4  -> target 4
+ * 1: 1, 2, 2, 4:1, 8:1
+ * total:2
+ * 
+ * 
  *
  */
 public class PathSum3 {
 
-	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	
+	Map<Long, Integer> map = new HashMap<>();
+	int count = 0;
 	
     public int pathSum(TreeNode root, int targetSum) {
-
+    	search(root, targetSum, 0L);
+    	return count;
     }
+    
+    public void search(TreeNode root, int targetSum, long currSum) {
+    	if(root==null) {
+    		return;
+    	}
+    	
+    	currSum += root.val;
+    	
+    	if(currSum == targetSum) {
+    		count++;
+    	}
+   
+    	long more = currSum-targetSum;
+    	if(map.containsKey(more)) {
+    		count += map.get(more);
+    	} 
+    	
+    	map.put(currSum, (map.getOrDefault(currSum, 0)+1));
+    	
+    	search(root.left, targetSum, currSum);
+    	search(root.right, targetSum, currSum);
+    	
+    	map.put(currSum, (map.get(currSum)-1));
+    	
+    }
+    
+
+    
+    
 
 
 }
