4f908f404dbb3de56ca3f45d9f48aa569ea4af8e
Codi Chun <codichun@uw.edu>
2022-11-28 11:30:00 -0800

    add ClimbingStairs

--- src/dp/ClimbingStairs.java
+++ src/dp/ClimbingStairs.java
@@ -1,8 +1,14 @@
 package dp;
 
 /**
+ * dp[n] = dp[n-1] + dp[n-2]
  * 
+ * 1 -> 1
+ * 2 -> 2
+ * 3 -> dp[2] + dp[1]
  *
+ *time:o(n)
+ *space:o(n)
  */
 
 public class ClimbingStairs {
@@ -14,7 +20,22 @@ public class ClimbingStairs {
 	}
 	
     public int climbStairs(int n) {
+    	
+    	if(n==1) {
+    		return 1;
+    	}
+    	
+        int[] dp = new int[n+1];
         
+        dp[1] = 1;
+        dp[2] = 2;
+
+        
+        for(int i=3; i<=n; i++) {
+        	dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
     }
 
 }
