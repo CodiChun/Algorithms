5ca3e8c24cc9bbc6ae9b308ecce244f505092b4f
Codi Chun <codichun@uw.edu>
2022-12-04 09:48:11 -0800

    add BestTimetoBuyandSellStockwithCooldown

--- src/arrays/BestTimetoBuyandSellStockwithCooldown.java
+++ src/arrays/BestTimetoBuyandSellStockwithCooldown.java
@@ -9,28 +9,31 @@ public class BestTimetoBuyandSellStockwithCooldown {
 	
     /*
      *      1,2,4
+     *      
+	     * presold = sold
+	     * sold = held + prices[i]
+	     * held = max(held, reset - prices[i])
+	     * reset = max(reset, presold)
      * 
-     * buy
-     * sell
-     * hold
-     * cooldown
-     * 
-     *  1,2,4
-     * 
-     * 
-     * 
-	     * preSold = sold
-	     * sold(held[i-1] + price[i])
-	     * held: max(held, reset - price[i])
-	     * reset: max(reset, preSold)
      * max(sold, reset)
      * 
+     * time:o(n)
+     * space: o(1)
      * 
     */
 	
     public int maxProfit(int[] prices) {
     	
+    	int sold = Integer.MIN_VALUE, held = Integer.MIN_VALUE, reset = 0;
     	
+    	for(int p : prices) {
+    		int presold = sold;
+    		sold = held + p;
+    		held = Math.max(held, reset - p);
+    		reset = Math.max(reset, presold);
+    	}
+    	
+    	return Math.max(sold, reset);
     	
 
     }
---
+++ src/strings/MultiplyStrings.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class MultiplyStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public String multiply(String num1, String num2) {
+        
+    }
+
+}
